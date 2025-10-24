ba6bb0af801eb97d6044a7de4ada18fb1dd5c007
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-24 19:36:43 +0600

    add comment out sms

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,23 +3,23 @@ import { TiThMenu } from "react-icons/ti";
 import { IoMdCloseCircle } from "react-icons/io";
 
 const Navbar = () => {
+    // ============  useState  + Function ================= 
     const [icon, setIcon] = useState(false);
     const toggleMenu = () =>{
         setIcon(!icon);
     }
+    
     return (
         <nav className='bg-green-600 text-white py-4 md:py-6'>
-
             <div className=' container mx-auto flex   justify-between items-center '>
                 <h1 className='text-2xl'>Logo</h1>
-                {/* =========================     Mobile Menu Btn   ============================ */}
 
+                {/* =========================     Mobile Menu Btn   ============================ */}
                 <div>
                     <button onClick={toggleMenu}>
                         {
                             icon ? <IoMdCloseCircle /> :   <TiThMenu /> 
-                        }
-                       
+                        }                      
                         
                     </button>
                 </div>
