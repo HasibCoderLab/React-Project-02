6440472106af7fc1d9833079442db986be65b7a7
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-25 21:02:57 +0600

    set BG img

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col bg-green-500  text-white text-center justify-center h-[81vh]'>
+    <div className='flex flex-col bg-green-500  text-white px-4 items-center text-center justify-center h-[81vh]' style={{
+        backgroundImage: "url('favicon.jpeg')",
+      backgroundPosition:"center",
+      backgroundSize:"cover"
+    }} >
         <h1 className='font-bold text-4xl mb-4 md:text-6xl '>Welcome My Website</h1>
-        <p>Start</p>
-        <button>Buy Now</button>
+        <p className=' text-lg  md:text-xl   mb-4'> Start</p>
+        <button className=' bg-white text-violet-600  font-bold px-6 py-2 hover:bg-gray-200 rounded-md cursor-pointer '>Buy Now</button>
     </div>
   )
 }
