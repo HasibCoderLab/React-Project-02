5a41c37eb919bc267dcc53432a1ba2140e32d5ed
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-25 22:11:02 +0600

    Products section

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+  import React from 'react'
+;
 
-const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+  const Products = () => {
+    return (
+      <div className='flex flex-col bg-green-400 text-white px-4 items-center text-center justify-center h-[91vh] md:h-[82vh]' 
+       >
+        <h1 className='font-bold text-4xl mb-4 md:text-6xl '>Welcome Products Section </h1>
+        <p className=' text-lg  md:text-xl   mb-4'> Visit my website  & explore with enjoy</p>
+      </div>
+    )
+  }
 
-export default Products
+  export default Products
+
+
+
+
