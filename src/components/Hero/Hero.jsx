c2f503347539be27eafae775940631c1a0a68f4a
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-25 21:07:59 +0600

    fixed bg img

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+  import React from 'react'
 
-const Hero = () => {
-  return (
-    <div className='flex flex-col bg-green-500  text-white px-4 items-center text-center justify-center h-[81vh]' style={{
+  const Hero = () => {
+    return (
+      <div className='flex flex-col bg-green-500  text-white px-4 items-center text-center justify-center h-[85vh] md:h-[82vh]' style={{
         backgroundImage: "url('favicon.jpeg')",
-      backgroundPosition:"center",
-      backgroundSize:"cover"
-    }} >
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }} >
         <h1 className='font-bold text-4xl mb-4 md:text-6xl '>Welcome My Website</h1>
-        <p className=' text-lg  md:text-xl   mb-4'> Start</p>
+        <p className=' text-lg  md:text-xl   mb-4'> Visit my website  & enjoy</p>
         <button className=' bg-white text-violet-600  font-bold px-6 py-2 hover:bg-gray-200 rounded-md cursor-pointer '>Buy Now</button>
-    </div>
-  )
-}
+      </div>
+    )
+  }
 
-export default Hero
+  export default Hero
