57cf71cfd3a25097b0c4d8ef998c5997a854aa8b
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-25 22:08:07 +0600

    update <h1> + <p> tag

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,9 +5,8 @@
     return (
       <div className='flex flex-col bg-green-400 text-white px-4 items-center text-center justify-center h-[91vh] md:h-[82vh]' 
        >
-        <h1 className='font-bold text-4xl mb-4 md:text-6xl '>Welcome My Website</h1>
-        <p className=' text-lg  md:text-xl   mb-4'> Visit my website  & enjoy</p>
-        <button className=' bg-white text-violet-600  font-bold px-6 py-2 hover:bg-gray-200 rounded-md cursor-pointer '>Buy Now</button>
+        <h1 className='font-bold text-4xl mb-4 md:text-6xl '>Welcome About Section </h1>
+        <p className=' text-lg  md:text-xl   mb-4'> Visit my website  & explore with enjoy</p>
       </div>
     )
   }
