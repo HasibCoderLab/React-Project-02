0f88ea0208b582804b86c24fde6cb69822504be2
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-25 22:12:06 +0600

    Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+  import React from 'react'
+;
 
-const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const Contact = () => {
+    return (
+      <div className='flex flex-col bg-green-400 text-white px-4 items-center text-center justify-center h-[91vh] md:h-[82vh]' 
+       >
+        <h1 className='font-bold text-4xl mb-4 md:text-6xl '>Welcome Contact Section </h1>
+        <p className=' text-lg  md:text-xl   mb-4'> Visit my website  & explore with enjoy</p>
+      </div>
+    )
+  }
 
-export default Contact
+  export default Contact
+
+
+
+
+
+ 
