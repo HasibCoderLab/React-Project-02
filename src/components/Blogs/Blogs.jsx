ee40e1bb7fd0ee87f9e9e96767d6184e98546214
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-25 22:09:35 +0600

    added code for Blogs section

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+  import React from 'react'
+;
 
-const Blogs = () => {
-  return (
-    <div>Blogs</div>
-  )
-}
+  const Blogs = () => {
+    return (
+      <div className='flex flex-col bg-green-400 text-white px-4 items-center text-center justify-center h-[91vh] md:h-[82vh]' 
+       >
+        <h1 className='font-bold text-4xl mb-4 md:text-6xl '>Welcome Blogs Section </h1>
+        <p className=' text-lg  md:text-xl   mb-4'> Visit my website  & explore with enjoy</p>
+      </div>
+    )
+  }
 
-export default Blogs
+  export default Blogs
