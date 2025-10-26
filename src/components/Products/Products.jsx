9c568db40982450d1e85d5f9195c4ff1d7fadbd5
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-26 07:55:40 +0600

    update bg color

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,7 +3,7 @@
 
   const Products = () => {
     return (
-      <div className='flex flex-col bg-green-400 text-white px-4 items-center text-center justify-center h-[91vh] md:h-[82vh]' 
+      <div className='flex flex-col bg-emerald-500 text-white px-4 items-center text-center justify-center h-[91vh] md:h-[82vh]' 
        >
         <h1 className='font-bold text-4xl mb-4 md:text-6xl '>Welcome Products Section </h1>
         <p className=' text-lg  md:text-xl   mb-4'> Visit my website  & explore with enjoy</p>
